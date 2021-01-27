08d61d93c60f658628c09b7f0008d4c47fb693a6
Kamesuta <kamesuta@gmail.com>
2021-01-27 17:16:14 +0900

    mildom

--- youtube_dl/extractor/mildom.py
+++ youtube_dl/extractor/mildom.py
@@ -31,7 +31,7 @@ class MildomIE(InfoExtractor):
         'only_matching': True,
     }]
 
-    _VALID_URL = r'https?://(?:www\.|m\.)?mildom\.com/playback/(?P<user>[0-9]+)/(?P<id>[0-9]+-[0-9a-z]+)'
+    _VALID_URL = r'https?://(?:www\.|m\.)?mildom\.com/(?P<id>[0-9]+)'
     _NETRC_MACHINE = 'mildom'
 
     def _real_extract(self, url):
@@ -40,33 +40,37 @@ class MildomIE(InfoExtractor):
         # Get video webpage. We are not actually interested in it for normal
         # cases, but need the cookies in order to be able to download the
         # info webpage
-        webpage_url = 'http://www.mildom.com/playback/' + video_id
+        webpage_url = 'http://www.mildom.com/' + video_id
         webpage, handle = self._download_webpage_handle(webpage_url, video_id)
 
         response = self._download_json(
-            'https://cloudac.mildom.com/nonolive/videocontent/playback/getPlaybackDetail?v_id=%s' % video_id, video_id,
+            'https://cloudac.mildom.com/nonolive/gappserv/live/enterstudio?__platform=web&user_id=%s' % video_id, video_id,
             query={'_format': 'json'},
             headers={'Content-Type': 'application/json'},
             note='Downloading JSON metadata for %s' % video_id)
 
-        meta = response['body']['playback']
+        manifest = self._extract_f4m_formats(
+
+        )
+
+        meta = response['body']
 
         return {
             'id': video_id,
-            'title': meta['title'],
-            'formats': meta['video_link'],
-            'thumbnail': meta['upload_pic'],
-            'description': meta['video_intro'],
-            'uploader': meta['author_info']['login_name'],
-            'timestamp': meta['publish_time'],
+            'title': meta['anchor_intro'],
+            'formats': meta['ext'],
+            'thumbnail': meta['pic'],
+            'description': meta['live_intro'],
+            'uploader': meta['loginname'],
+            'timestamp': meta['live_start_ms'],
             'uploader_id': meta['user_id'],
-            'view_count': meta['view_num'],
-            'duration': meta['video_length'],
+            'view_count': meta['viewers'],
+            'duration': meta['live_start_ms'] - response['ts_ms'],
             'webpage_url': webpage_url,
         }
 
 
-class MildomIE(InfoExtractor):
+class MildomPlaybackIE(InfoExtractor):
     IE_NAME = 'mildom'
     IE_DESC = 'ミルダム'
 
